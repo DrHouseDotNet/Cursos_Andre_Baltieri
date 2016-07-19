a68fcaa1bcba4bc3c4a7e687bb5cfee552bf6d50
sidneipsj <sidneipsj@gmail.com>
2016-07-18 23:20:25 -0300

    Brincando com validators

---
+++ Sidnei - Curso1939/Sidnei - Curso1939/Controllers/AlunoController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sidnei___Curso1939.Models;
+
+namespace Sidnei___Curso1939.Controllers
+{
+    [RoutePrefix("alunos")]
+    public class AlunoController : Controller
+    {
+        // GET: Aluno
+        [Route("listar")]
+        public ActionResult Index()
+        {
+            Aluno sidnei = new Aluno
+            {
+                AlunoId = 1,
+                CPF = "383",
+                Email = "sidneipsj@gmail.com",
+                Nome = "Sidnei Sanches"
+            };
+            IList<Aluno> alunos = new List<Aluno>();
+            alunos.Add(sidnei);
+           
+            
+
+            return View(alunos);
+        }
+
+        [Route("criar")]
+        public ActionResult Create()
+        {
+            ModelState.AddModelError("Mensagem", "Algum erro ocorreu");
+            return View();
+        }
+
+        [Route("criar")]
+        [HttpPost]
+        public ActionResult Create(Aluno aluno)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Sidnei - Curso1939/Sidnei - Curso1939/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sidnei___Curso1939.MvcApplication" Language="C#" %>
